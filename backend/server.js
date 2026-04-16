4191ed87f18eb76c438a885b36ad2d5155d6cbc0
hanjangweb <omitterang@gmail.com>
2026-04-16 06:25:23 +0530

    Changes updated

--- backend/server.js
+++ backend/server.js
@@ -165,7 +165,7 @@ app.post("/api/chat", async (req, res) => {
 // --------------------------- */
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("(.*)", (req, res) => {
+app.get("*path", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
